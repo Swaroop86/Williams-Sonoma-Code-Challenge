657d4f198abe9adb1f105b41dd1caf9b3575390a
Swaroop86 <sbrain2016@gmail.com>
2019-06-14 03:54:33 -0700

    Update ZipCodeValidatorTest.java

--- src/test/java/com/ws/code/challenge/util/ZipCodeValidatorTest.java
+++ src/test/java/com/ws/code/challenge/util/ZipCodeValidatorTest.java
@@ -1,43 +1,82 @@
-package com.ws.code.challenge.util;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class ZipCodeValidatorTest {
+/**
+ * Test class for FileUtil
+ * @author Swaroop Shivarudrappa
+ *
+ */
+public class FileUtilTest {
 	
 	@Test
-	public void testValidZipRanges() {
-
-		Assert.assertEquals(true, ZipCodeValidator.isValidZipCodeRange("[94133,94166]"));
-		Assert.assertEquals(true, ZipCodeValidator.isValidZipCodeRange("[94200,94299]"));
-	}
-	
 	/**
-	 * Test various invalid zip code strings.
+	 * Method to test parse Input,  created test.txt file and the zipcodes are mapped to zipcode range objects
+	 * 
 	 */
-	@Test
-	public void testInvalidZipRanges() {
-		//Invalid pattern
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("[94133,83jdhjd]"));
+	public void testParseInput() {
+		String fileName = "test.txt";
+		List<List<ZipCodeRange>> expected = new ArrayList<>();
+		List<ZipCodeRange> expected1 = new ArrayList<>();
+		List<ZipCodeRange> expected2 = new ArrayList<>();
+		ZipCodeRange zipExp1 = new ZipCodeRange(94133,94133);
+		ZipCodeRange zipExp2 = new ZipCodeRange(94200,94299);
+		ZipCodeRange zipExp3 = new ZipCodeRange(94400,94600);
+		expected1.add(zipExp1);
+		expected1.add(zipExp2);
+		expected1.add(zipExp3);
+		ZipCodeRange zipExp4 = new ZipCodeRange(94133,94133);
+		ZipCodeRange zipExp5 = new ZipCodeRange(94300,94399);
+		ZipCodeRange zipExp6 = new ZipCodeRange(94500,94900);
+		expected2.add(zipExp4);
+		expected2.add(zipExp5);
+		expected2.add(zipExp6);
+		expected.add(expected1);
+		expected.add(expected2);
+		List<List<ZipCodeRange>> actual = FileUtil.parseInput(fileName);
 		
-		//Above 5 digit zipcode
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("[941335,947890"));
-		
-		//Missing brackets
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("9427894568]"));
-		
-		//Empty input
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("[]"));
-		
-		//missing upper bound
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("[94200,]"));
-				
-		//missing lower bound
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange("[,94299]"));
-		
-		//special characters
-		Assert.assertEquals(false, ZipCodeValidator.isValidZipCodeRange(null));
-		
+		Assert.assertEquals(expected, actual);
 	}
+	
+	@Test
+	/**
+	 * Method to test validation and the mapping to ZipCodeRange objects
+	 */
+	public void testValidateAndMap() {
+		String zipRange1 = new String("[94133,94133] [94200,94299] [94400,94600]");
+		String zipRange2 = new String("[94133,94133] [94300,94399] [94500,94900]");
+		List<String> inputs = new ArrayList<>();
+		inputs.add(zipRange1);
+		inputs.add(zipRange2);
 
-}
+		List<ZipCodeRange> expected1 = new ArrayList<>();
+		List<ZipCodeRange> expected2 = new ArrayList<>();
+		ZipCodeRange zipExp1 = new ZipCodeRange(94133,94133);
+		ZipCodeRange zipExp2 = new ZipCodeRange(94200,94299);
+		ZipCodeRange zipExp3 = new ZipCodeRange(94400,94600);
+		expected1.add(zipExp1);
+		expected1.add(zipExp2);
+		expected1.add(zipExp3);
+		
+		ZipCodeRange zipExp4 = new ZipCodeRange(94133,94133);
+		ZipCodeRange zipExp5 = new ZipCodeRange(94300,94399);
+		ZipCodeRange zipExp6 = new ZipCodeRange(94500,94900);
+		expected2.add(zipExp4);
+		expected2.add(zipExp5);
+		expected2.add(zipExp6);
+		
+		List<List<ZipCodeRange>> expected = new ArrayList<>();
+		expected.add(expected1);
+		expected.add(expected2);
+		
+		List<List<ZipCodeRange>> actual = FileUtil.validateAndMap(inputs);
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	/**
+	 * Test for invalid inputs
+	 */
+	public void testValidAndMapError() {
+		String zipRange1 = new String("94133,94133] [94200,94299] [94400,94600]");
+		List<String> inputs = new ArrayList<>();
+		inputs.add(zipRange1);
+		FileUtil.validateAndMap(inputs);
+	}
