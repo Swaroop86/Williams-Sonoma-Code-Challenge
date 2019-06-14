e95e5765e87d447423e0bde89ec1b44b279fc8ab
Swaroop86 <sbrain2016@gmail.com>
2019-06-14 03:56:33 -0700

    Update ZipCodeValidatorTest.java

--- src/test/java/com/ws/code/challenge/util/ZipCodeValidatorTest.java
+++ src/test/java/com/ws/code/challenge/util/ZipCodeValidatorTest.java
@@ -1,6 +1,7 @@
 package com.ws.code.challenge.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
@@ -8,88 +9,44 @@ import org.junit.Test;
 
 import com.ws.code.challenge.model.ZipCodeRange;
 
-
 /**
- * Test class for FileUtil
+ * Test class to test the ZipCodeRangeComparator
+ * 
  * @author Swaroop Shivarudrappa
  *
  */
-public class FileUtilTest {
+public class ZipCodeRangeComparatorTest {
 	
 	@Test
 	/**
-	 * Method to test parse Input,  created test.txt file and the zipcodes are mapped to zipcode range objects
-	 * 
+	 * Method tests if the input gets sorted
 	 */
-	public void testParseInput() {
-		String fileName = "test.txt";
-		List<List<ZipCodeRange>> expected = new ArrayList<>();
-		List<ZipCodeRange> expected1 = new ArrayList<>();
-		List<ZipCodeRange> expected2 = new ArrayList<>();
-		ZipCodeRange zipExp1 = new ZipCodeRange(94133,94133);
-		ZipCodeRange zipExp2 = new ZipCodeRange(94200,94299);
-		ZipCodeRange zipExp3 = new ZipCodeRange(94400,94600);
-		expected1.add(zipExp1);
-		expected1.add(zipExp2);
-		expected1.add(zipExp3);
-		ZipCodeRange zipExp4 = new ZipCodeRange(94133,94133);
-		ZipCodeRange zipExp5 = new ZipCodeRange(94300,94399);
-		ZipCodeRange zipExp6 = new ZipCodeRange(94500,94900);
-		expected2.add(zipExp4);
-		expected2.add(zipExp5);
-		expected2.add(zipExp6);
-		expected.add(expected1);
-		expected.add(expected2);
-		List<List<ZipCodeRange>> actual = FileUtil.parseInput(fileName);
-		
-		Assert.assertEquals(expected, actual);
-	}
-	
-	@Test
-	/**
-	 * Method to test validation and the mapping to ZipCodeRange objects
-	 */
-	public void testValidateAndMap() {
-		String zipRange1 = new String("[94133,94133] [94200,94299] [94400,94600]");
-		String zipRange2 = new String("[94133,94133] [94300,94399] [94500,94900]");
-		List<String> inputs = new ArrayList<>();
-		inputs.add(zipRange1);
-		inputs.add(zipRange2);
+	public void testSort() {
+		ZipCodeRange range1 = new ZipCodeRange(94133, 94166);
+		ZipCodeRange range2 = new ZipCodeRange(96400, 96800);
+		ZipCodeRange range3 = new ZipCodeRange(95400, 95600);
+		ZipCodeRange range4 = new ZipCodeRange(92100, 92500);
 
-		List<ZipCodeRange> expected1 = new ArrayList<>();
-		List<ZipCodeRange> expected2 = new ArrayList<>();
-		ZipCodeRange zipExp1 = new ZipCodeRange(94133,94133);
-		ZipCodeRange zipExp2 = new ZipCodeRange(94200,94299);
-		ZipCodeRange zipExp3 = new ZipCodeRange(94400,94600);
-		expected1.add(zipExp1);
-		expected1.add(zipExp2);
-		expected1.add(zipExp3);
 		
-		ZipCodeRange zipExp4 = new ZipCodeRange(94133,94133);
-		ZipCodeRange zipExp5 = new ZipCodeRange(94300,94399);
-		ZipCodeRange zipExp6 = new ZipCodeRange(94500,94900);
-		expected2.add(zipExp4);
-		expected2.add(zipExp5);
-		expected2.add(zipExp6);
+		List<ZipCodeRange> ranges = new ArrayList<ZipCodeRange>();
 		
-		List<List<ZipCodeRange>> expected = new ArrayList<>();
-		expected.add(expected1);
-		expected.add(expected2);
+		ranges.add(range1);
+		ranges.add(range2);
+		ranges.add(range3);
+		ranges.add(range4);
+
 		
-		List<List<ZipCodeRange>> actual = FileUtil.validateAndMap(inputs);
+		Collections.sort(ranges, new ZipCodeRangeComparator());
 		
-		Assert.assertEquals(expected, actual);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	/**
-	 * Test for invalid inputs
-	 */
-	public void testValidAndMapError() {
-		String zipRange1 = new String("94133,94133] [94200,94299] [94400,94600]");
-		List<String> inputs = new ArrayList<>();
-		inputs.add(zipRange1);
-		FileUtil.validateAndMap(inputs);
+		Assert.assertEquals(92100, ranges.get(0).getLowerBound());
+		Assert.assertEquals(92500, ranges.get(0).getUpperBound());
+		Assert.assertEquals(94133, ranges.get(1).getLowerBound());
+		Assert.assertEquals(94166, ranges.get(1).getUpperBound());
+		Assert.assertEquals(95400, ranges.get(2).getLowerBound());
+		Assert.assertEquals(95600, ranges.get(2).getUpperBound());
+		Assert.assertEquals(96400, ranges.get(3).getLowerBound());
+		Assert.assertEquals(96800, ranges.get(3).getUpperBound());
+		
 	}
 
 }
